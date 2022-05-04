927b81ade1a16800bfe4f8f4d6551044b5564a90
Emiliano <emi.perez997@gmail.com>
2022-05-04 13:35:32 -0300

    [ADD] Proyecto Restify: Rutas Profile

--- clase-1/projecto-restify/src/service/routes/index.ts
+++ clase-1/projecto-restify/src/service/routes/index.ts
@@ -1,18 +1,13 @@
 import { Router } from 'restify-router';
 // import UserRoute from './user.routes';
-// import ProfileRoute from './profile.routes';
+import ProfileRoute from './profile.routes';
 
 const routerInstance = new Router();
-// const listOfRouter = new Router();
+const listOfRouter = new Router();
 
-// listOfRouter.add('/profile', ProfileRoute);
+listOfRouter.add('/profile', ProfileRoute);
 // listOfRouter.add('/user', UserRoute);
 
-// routerInstance.add('/app/v1', listOfRouter)
-
-// Prueba de endpoint
-routerInstance.get('/', async (req, res) => {
-  return res.json({message: 'Hola'})
-});
+routerInstance.add('/api/v1', listOfRouter)
 
 export default routerInstance;
